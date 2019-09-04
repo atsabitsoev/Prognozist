1f152c4b4234fc2cf63f5203854703b7a1fd50c5
Ацамаз Бицоев <atsamazbitsoev@yandex.ru>
2019-09-04 21:36:04 +0300

    сщььше

--- Prognozist/Subscribtion/View+Controller/SubscribtionVC.swift
+++ Prognozist/Subscribtion/View+Controller/SubscribtionVC.swift
@@ -11,6 +11,9 @@ import UIKit
 class SubscribtionVC: UIViewController {
     
     
+    @IBOutlet weak var viewWeekSubscribtion: UIButton!
+    @IBOutlet weak var viewMonthSubscribtion: UIButton!
+    @IBOutlet weak var viewYearSubscribtion: UIButton!
     
 
     override func viewDidLoad() {
@@ -19,12 +22,25 @@ class SubscribtionVC: UIViewController {
         setGradientBackground()
     }
     
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        setRoundedViewsSubscribtions()
+    }
+    
     
     private func setGradientBackground() {
         
         let color1 = Colors.darkBlueColor.cgColor
         let color2 = Colors.lightBlueColor.cgColor
         self.view.addGradient(colors: [color2, color1])
+    }
+    
+    private func setRoundedViewsSubscribtions() {
+        
+        viewWeekSubscribtion.layer.cornerRadius = viewWeekSubscribtion.bounds.height / 2
+        viewMonthSubscribtion.layer.cornerRadius = viewMonthSubscribtion.bounds.height / 2
+        viewYearSubscribtion.layer.cornerRadius = viewYearSubscribtion.bounds.height / 2
     }
     
     
